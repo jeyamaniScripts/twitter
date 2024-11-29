b7ae05be956272b7e188683a580d49169a3d18e7
jeyamaniScripts <thejeyamani034@gmail.com>
2024-11-29 22:12:21 +0530

    setup daisyui theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,21 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-  ],
-}
-
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "light",
+      {
+        black: {
+          ...require("daisyui/src/theming/themes")["black"],
+          primary: "blue", //"rgb(29,155,240)"
+          secondary: "teal", //"rgb(24,24,24)"
+        },
+      },
+    ],
+  },
+};

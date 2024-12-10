509b833e514886d96f466702ebaea36c37c3f713
jeyamaniScripts <thejeyamani034@gmail.com>
2024-12-10 14:06:04 +0530

    create home page ui and createpost page then post page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,44 @@
 // import React from "react";
 
+import { useState } from "react";
+import Post from "../../components/commen/Post";
+import CreatePost from "./CreatePost";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [feedType, setFeedType] = useState("forYou");
+
+  const handleFeedType = (type) => {
+    setFeedType(type);
+  };
+  return (
+    <div className="min-h-screen flex-[4_4_0] mr-auto border-r border-gray-700">
+      {/* Header  */}
+      <div className="flex w-full border-b border-gray-700">
+        <div
+          className="flex justify-center flex-1 relative cursor-pointer p-3 hover:bg-secondary transition duration-300"
+          onClick={() => handleFeedType("forYou")}
+        >
+          For You
+          {feedType === "forYou" && (
+            <div className=" absolute bg-primary h-1 w-10 bottom-0  rounded-full"></div>
+          )}
+        </div>
+        <div
+          className="flex justify-center flex-1 relative cursor-pointer p-3 hover:bg-secondary transition duration-300"
+          onClick={() => handleFeedType("following")}
+        >
+          Following
+          {feedType === "following" && (
+            <div className=" absolute bg-primary h-1 w-10 bottom-0  rounded-full"></div>
+          )}
+        </div>
+      </div>
+      {/* Create Post  */}
+      <CreatePost />
+      {/* Post  */}
+      <Post />
+    </div>
+  );
 };
 
 export default Home;
